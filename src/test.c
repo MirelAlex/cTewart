aca4176d8d08749c3a201a305270e2ec0f45a81a
Mirel Alex <>
2024-03-19 22:29:11 +0200

    Other unimportat updates (easily can be skipped)

--- src/test.c
+++ src/test.c
@@ -14,6 +14,9 @@ typedef struct {
     float zoom;
     Vector2 offset;
     bool dragging;
+    int lastX;
+    int lastY;
+    bool zoomChanged;
 } PlotWidget;
 
 void InitializePlotWidget(PlotWidget* widget, Rectangle rect) {
@@ -24,6 +27,10 @@ void InitializePlotWidget(PlotWidget* widget, Rectangle rect) {
     widget->offset.x = 0.0f;
     widget->offset.y = 0.0f;
     widget->dragging = false;
+    widget->lastX = 0;
+    widget->lastY = 0;
+    widget->zoomChanged = false;
+
 }
 
 void DrawScaleLabels(Rectangle plotRect, int interval, float zoom)
@@ -65,7 +72,6 @@ void UpdatePlotWidget(PlotWidget* widget, float inputValue) {
     // Store unaltered input value in buffer
     widget->buffer[widget->historyIndex] = inputValue;
 
-    // Calculate new sine wave value
     float newValuePlot = inputValue / widget->zoom;
 
     // Update history array
@@ -73,39 +79,56 @@ void UpdatePlotWidget(PlotWidget* widget, float inputValue) {
     widget->historyIndex = (widget->historyIndex + 1) % MAX_HISTORY;
 
     // Rescale history values with the new zoom level FAIL
-    // for (int i = widget->historyIndex; i < MAX_HISTORY; i++) {
-    //     if (widget->zoom != 0)
-    //     {
-    //         /* code */
-    //         widget->history[i] = (int)(widget->history[i] / widget->zoom);
+    // if (widget->zoomChanged)
+    // {
+    //     for (int i = widget->historyIndex+1; i < MAX_HISTORY; i++) {
+    //         if (widget->zoom != 0)
+    //         {
+    //             widget->history[i] = (int)(widget->history[i] / widget->zoom);
+    //         }
     //     }
-
+    //     widget->zoomChanged = false;
     // }
+
 }
 
 void DrawPlotWidget(PlotWidget* widget) {
-    // Draw the movable rectangle
-    DrawRectangleLinesEx(widget->rect, 2, BLUE);
 
     // Draw scale labels along y-axis
     DrawScaleLabels(widget->rect, widget->interval, widget->zoom);
-
+    float x1, y1;
+    float x2, y2;
     // Draw the scaled sine wave within the rectangle
     for (int i = 0; i < MAX_HISTORY - 1; i++)
     {
         // Calculate positions of the points
-        float x1 = i * (widget->rect.width - 1) / (MAX_HISTORY - 1) + widget->rect.x + 1;
-        float y1 = widget->rect.y + RECT_HEIGHT / 2 - widget->history[(widget->historyIndex + i) % MAX_HISTORY];
-        float x2 = (i + 1) * (widget->rect.width - 1) / (MAX_HISTORY - 1) + widget->rect.x + 1;
-        float y2 = widget->rect.y + RECT_HEIGHT / 2 - widget->history[(widget->historyIndex + i + 1) % MAX_HISTORY];
+        x1 = i * (widget->rect.width - 1) / (MAX_HISTORY - 1) + widget->rect.x + 1;
+        y1 = widget->rect.y + RECT_HEIGHT / 2 - widget->history[(widget->historyIndex + i) % MAX_HISTORY];
+        x2 = (i + 1) * (widget->rect.width - 1) / (MAX_HISTORY - 1) + widget->rect.x + 1;
+        y2 = widget->rect.y + RECT_HEIGHT / 2 - widget->history[(widget->historyIndex + i + 1) % MAX_HISTORY];
 
-        // Draw line between points
-        if ((y1 > widget->rect.y) && (y2 > widget->rect.y) &&
-            (y1 < widget->rect.y+RECT_HEIGHT) && (y2 < widget->rect.y+RECT_HEIGHT))
-        {
-            DrawLine((int)x1, (int)y1, (int)x2, (int)y2, RED);
+        if (y1 < widget->rect.y){
+            y1 = widget->rect.y;
         }
+        if (y2 < widget->rect.y){
+            y2 = widget->rect.y;
+        }
+        if (y1 > widget->rect.y + RECT_HEIGHT){
+            y1 = widget->rect.y + RECT_HEIGHT;
+        }
+        if (y2 > widget->rect.y + RECT_HEIGHT){
+            y2 = widget->rect.y + RECT_HEIGHT;
+        }
+        // Draw line between points
+        DrawLine((int)x1, (int)y1, (int)x2, (int)y2, RED);
     }
+
+    widget->lastX = x1;
+    widget->lastY = y1;
+
+    // Draw the movable rectangle
+    DrawRectangleLinesEx(widget->rect, 2, BLUE);
+
 }
 
 void HandleMovingWidget(PlotWidget* w){
@@ -138,6 +161,7 @@ void HandleZoomingWidget(PlotWidget* w){
     if (CheckCollisionPointRec(mousePos, w->rect) && scroll != 0)
     {
         w->zoom += scroll * 0.1f; // Adjust zoom level
+        w->zoomChanged = true;
         if (w->zoom < 0.1f)
             w->zoom = 0.1f; // Prevent zoom level from becoming too small
     }
@@ -169,13 +193,17 @@ int main(void)
         HandleZoomingWidget(&widget1);
 
         // Update the widget with new input value
-        float inputValue = sinf(GetTime()*10) * (RECT_HEIGHT / 2) * 4/PI; //
+        float inputValue = sinf(GetTime()*10) * 10; //
         UpdatePlotWidget(&widget, inputValue);
+        DrawText(TextFormat("%.2f", inputValue),widget.lastX + 10, widget.lastY, 10, GRAY);
+        inputValue = sinf(GetTime()*10) * (RECT_HEIGHT / 2) * 4/PI;
         inputValue += sinf(GetTime()*30) * (RECT_HEIGHT / 6) * 4/PI; //
         inputValue += sinf(GetTime()*50) * (RECT_HEIGHT / 10) * 4/PI; //
         inputValue += sinf(GetTime()*70) * (RECT_HEIGHT / 14) * 4/PI; //
+        inputValue += sinf(GetTime()*90) * (RECT_HEIGHT / 19) * 4/PI; //
+        inputValue += sinf(GetTime()*110) * (RECT_HEIGHT / 21) * 4/PI; //
         UpdatePlotWidget(&widget1, inputValue);
-
+        DrawText(TextFormat("%.2f", inputValue),widget1.lastX + 10, widget1.lastY, 10, GRAY);
         // Draw
         BeginDrawing();
 
